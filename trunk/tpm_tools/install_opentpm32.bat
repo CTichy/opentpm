5fe3d276c4f2d5bbb5c39168e953f6d22a2683db
coreykal@gmail.com <coreykal@gmail.com@72c3de3a-a765-f184-7f5a-983784259fdc>
2013-09-21 21:42:04 +0000

    committing compiled versions of the libtpm utilities and the opentpm driver

---
+++ trunk/tpm_tools/install_opentpm32.bat
@@ -0,0 +1,2 @@
+sc create OpenTPM type= kernel start= auto binPath= C:\tpm_tools\opentpm32.sys
+sc start OpenTPM
---
+++ trunk/tpm_tools/install_opentpm64.bat
@@ -0,0 +1,2 @@
+sc create OpenTPM type= kernel start= auto binPath= C:\tpm_tools\opentpm64.sys
+sc start OpenTPM
